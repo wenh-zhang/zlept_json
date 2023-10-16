5b416ce14c22a894f283ce33e009e7708f65b853
zhangwenhua <2532175300@qq.com>
2023-10-16 10:14:43 +0800

    feat: extend unicode escape

--- zleptjson.c
+++ zleptjson.c
@@ -20,7 +20,7 @@
 #define PUTC(c, ch)                                     \
   do {                                                  \
     *(char*)zlept_context_push(c, sizeof(char)) = (ch); \
-  } while (0);
+  } while (0)
 
 typedef struct {
   const char* json;
@@ -108,9 +108,51 @@ static int zlept_parse_number(zlept_context* c, zlept_value* v) {
   return ZLEPT_PARSE_OK;
 }
 
+#define STRING_ERROR(ret) \
+  do {                    \
+    c->top = head;        \
+    return ret;           \
+  } while (0)
+
+static const char* zlept_parse_hex4(const char* p, unsigned* u) {
+  int i;
+  *u = 0;
+  for(i = 0; i < 4; i++){
+    char ch = *p++;
+    *u <<= 4;
+    if(ch >= '0' && ch <= '9') *u |= ch - '0';
+    else if(ch >= 'A' && ch <= 'F') *u |= ch - 'A' + 10;
+    else if(ch >= 'a' && ch <= 'f') *u |= ch - 'a' + 10;
+    else return NULL;
+  }
+  return p;
+}
+
+static void zlept_encode_utf8(zlept_context* c, unsigned u) {
+  if(u <= 0x007F)
+    PUTC(c, u & 0x7F);
+  else if(u <= 0x07FF){
+    PUTC(c, 0xC0 | ((u >> 6) & 0xFF));
+    PUTC(c, 0x80 | (u & 0x3F));
+  }
+  else if(u <= 0xFFFF){
+    PUTC(c, 0xE0 | ((u >> 12) & 0xFF));
+    PUTC(c, 0x80 | ((u >> 6) & 0x3F));
+    PUTC(c, 0x80 | (u & 0x3F));
+  }
+  else{
+    assert(u <= 0x10FFFF);
+    PUTC(c, 0xF0 | ((u >> 18) & 0xFF));
+    PUTC(c, 0x80 | ((u >> 12) & 0x3F));
+    PUTC(c, 0x80 | ((u >> 6) & 0x3F));
+    PUTC(c, 0x80 | (u & 0x3F));
+  }
+}
+
 static int zlept_parse_string(zlept_context* c, zlept_value* v) {
   size_t head = c->top, len;
   const char* p;
+  unsigned u, u2;
   EXPECT(c, '\"');
   p = c->json;
   for (;;) {
@@ -142,9 +184,26 @@ static int zlept_parse_string(zlept_context* c, zlept_value* v) {
           case 't':
             PUTC(c, '\t');
             break;
+          case 'u':
+            if (!(p = zlept_parse_hex4(p, &u)))
+              STRING_ERROR(ZLEPT_PARSE_INVALID_UNICODE_HEX);
+            if(u >= 0xD800 && u <= 0xDBFF){
+              if(*p++ != '\\'){
+                STRING_ERROR(ZLEPT_PARSE_INVALID_UNICODE_SURROGATE);
+              }
+              if(*p++ != 'u'){
+                STRING_ERROR(ZLEPT_PARSE_INVALID_UNICODE_SURROGATE);
+              }
+              if (!(p = zlept_parse_hex4(p, &u2)))
+                STRING_ERROR(ZLEPT_PARSE_INVALID_UNICODE_HEX);
+              if(u2 < 0xDC00 || u2 > 0xDFFF)
+                STRING_ERROR(ZLEPT_PARSE_INVALID_UNICODE_SURROGATE);
+              u = (((u - 0xD800) << 10) | (u2 - 0xDC00)) + 0x10000;
+            }
+            zlept_encode_utf8(c, u);
+            break;
           default:
-            c->top = head;
-            return ZLEPT_PARSE_INVALID_STRING_ESCAPE;
+            STRING_ERROR(ZLEPT_PARSE_INVALID_STRING_ESCAPE);
         };
         break;
       case '\"':
@@ -153,14 +212,12 @@ static int zlept_parse_string(zlept_context* c, zlept_value* v) {
         c->json = p;
         return ZLEPT_PARSE_OK;
       case '\0':
-        c->top = head;
-        return ZLEPT_PARSE_MISS_QUOTATION_MARK;
+        STRING_ERROR(ZLEPT_PARSE_MISS_QUOTATION_MARK);
       default:
         if ((unsigned char)ch < 0x20) {
-          c->top = head;
-          return ZLEPT_PARSE_INVALID_STRING_CHAR;
+          STRING_ERROR(ZLEPT_PARSE_INVALID_STRING_CHAR);
         }
-        PUTC(c, ch)
+        PUTC(c, ch);
     }
   }
 }
